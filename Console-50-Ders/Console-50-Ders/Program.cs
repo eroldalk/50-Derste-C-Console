3a7c651e1922c1460b083696df2015cf5637b8de
Erol Dalkılıç <eroldalg@gmail.com>
2022-11-29 14:26:53 +0300

    hello world

---
+++ Console-50-Ders/Console-50-Ders/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_50_Ders
+{
+        class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();//readline writeline ı alt satıra taşır
+            //Console.Read(); read da writeta ne yazıldıysa o çıkar
+        }
+    }
+}
---
+++ Console-50-Ders/string-degiskenler/Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_50_Ders
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //string isimi;
+            //isimi = "Erol Dalkılıç";
+            //Console.WriteLine(isimi);
+            //Console.ReadLine();
+
+
+
+
+
+
+            //string isim;
+            //Console.Write("İsminizi Giriniz: ");
+            //isim = Console.ReadLine();
+            //Console.WriteLine("Hoşgeldiniz : " + isim);
+            //Console.ReadLine();
+
+
+
+
+
+            //string ad, soyad, yas, meslek, sehir;
+
+            //Console.WriteLine("lütfen sırasıyla Ad Soyad Yaş Meslek ve Şehrinizi Giriniz ....");
+
+            //ad=Console.ReadLine();
+            //soyad=Console.ReadLine();
+            //yas=Console.ReadLine();
+            //meslek=Console.ReadLine();
+            //sehir=Console.ReadLine();
+
+            //Console.WriteLine("Ad: " + ad);
+            //Console.WriteLine("Soyad: " + soyad);
+            //Console.WriteLine("Yaş: " + yas);
+            //Console.WriteLine("Meslek: " + meslek);
+            //Console.WriteLine("Şehir: " + sehir);
+
+
+
+
+
+
+
+
+            //string ad;
+
+            //Console.WriteLine("lütfen isminizi girin: ");
+
+            //ad = Console.ReadLine();
+
+            //Console.WriteLine("Merhaba: {0}", ad);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+            string ad, soyad, yas, meslek, sehir;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
